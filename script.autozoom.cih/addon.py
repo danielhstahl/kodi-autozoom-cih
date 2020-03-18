81103d357d9eb3c604148cb6983e4412461840ed
Daniel Stahl <danstahl1138@gmail.com>
2020-03-17 19:18:23 -0500

    added reset zoom

--- script.autozoom.cih/addon.py
+++ script.autozoom.cih/addon.py
@@ -5,11 +5,80 @@ import xbmcaddon
 import xbmc
 addon = xbmcaddon.Addon()
 addonname = addon.getAddonInfo('name')
+capture = xbmc.RenderCapture()
 ASPECT_RATIO_TO_ZOOM = ["1.33", "1.37", "1.66", "1.78", "1.85"]
 
+# capture Width & Height
+# each captured pixel is 40wx20h of the screen pixel
+# 1920 x 1080 = 1.77
+# 1920 x 1038 = 1.85 (20) (line 0 dark)
+# 1920 x 960 = 2.0 (60) (line 0 - 2 dark)
+# 1920 x 817 = 2.35 (130) (line 0 - 5 dark)
+CAPTURE_WIDTH = 48
+CAPTURE_HEIGHT = 54
 
-def getAspectRatio(xbmc):
-    return xbmc.getInfoLabel('VideoPlayer.VideoAspect')
+
+def CaptureFrame(xbmc, capture):
+    capture.capture(CAPTURE_WIDTH, CAPTURE_HEIGHT,
+                    xbmc.CAPTURE_FLAG_CONTINUOUS)
+
+    while (capture.getCaptureState() != xbmc.CAPTURE_STATE_DONE):
+        capture.waitForCaptureStateChangeEvent()
+
+        if (capture.getCaptureState() == xbmc.CAPTURE_STATE_DONE):
+            capturedImage = capture.getImage()
+            return capturedImage
+
+
+def LineColorLessThan(_bArray, _lineStart, _lineCount, _threshold):
+    __sliceStart = _lineStart * CAPTURE_WIDTH * 4
+    __sliceEnd = (_lineStart + _lineCount) * CAPTURE_WIDTH * 4
+
+    # zero out the alpha channel
+    i = __sliceStart + 3
+    while (i < __sliceEnd):
+        _bArray[i] &= 0x00
+        i += 4
+
+    __imageLine = _bArray[__sliceStart:__sliceEnd]
+    return all([v < _threshold for v in __imageLine])
+
+
+def GetAspectRatioFromFrame(xbmc, capture):
+    __aspectratio = capture.getAspectRatio()
+    __threshold = 25
+    xbmcAspectRatio = xbmc.getInfoLabel('VideoPlayer.VideoAspect')
+    # Analyze the frame only if the ratio is 16:9. 2.35 ratio files
+    # would not have black bars hardcoded.
+    if 1.40 < __aspectratio < 1.80:
+        # screen capture and test for an image that is not dark in the 2.40
+        # aspect ratio area. keep on capturing images until captured image
+        # is not dark
+        while (True):
+            __myimage = CaptureFrame(xbmc, capture)
+            __middleScreenDark = LineColorLessThan(
+                __myimage, 7, 2, __threshold)
+            if __middleScreenDark == False:
+                xbmc.sleep(200)
+                break
+            else:
+                xbmc.sleep(100)
+
+        # Capture another frame. after we have waited for transitions
+        __myimage = CaptureFrame(xbmc, capture)
+        __ar185 = LineColorLessThan(__myimage, 0, 1, __threshold)
+        __ar200 = LineColorLessThan(__myimage, 1, 3, __threshold)
+        __ar235 = LineColorLessThan(__myimage, 1, 5, __threshold)
+
+        if (__ar235 == True):
+            xbmcAspectRatio = "2.35"
+
+        elif (__ar200 == True):
+            xbmcAspectRatio = "2.00"
+
+        elif (__ar185 == True):
+            xbmcAspectRatio = "1.85"
+    return xbmcAspectRatio
 
 
 def setMasking(xbmc):
@@ -18,6 +87,7 @@ def setMasking(xbmc):
 
 
 ZOOM_LEVEL = 23
+ZOOM_RESET = 50
 
 
 def setZoomNonScope(xbmc):
@@ -25,24 +95,28 @@ def setZoomNonScope(xbmc):
     for _level in range(ZOOM_LEVEL):
         xbmc.executebuiltin('XBMC.Action(ZoomOut)')
 
+# hacky, but if already zoomed out this will work
+# I never zoom in, so this should reset
+
 
 def resetZoom(xbmc):
-    for _level in range(ZOOM_LEVEL):
+    for _level in range(ZOOM_RESET):
+        xbmc.executebuiltin('XBMC.Action(ZoomOut)')
+    for _level in range(ZOOM_RESET):
         xbmc.executebuiltin('XBMC.Action(ZoomIn)')
 
 
 def main(xbmc):
     xbmc.log('addon %s starting' % addonname)
+
     try:
-        aspect_ratio = getAspectRatio(xbmc)
+        resetZoom(xbmc)
+        aspect_ratio = GetAspectRatioFromFrame(xbmc, capture)
         xbmc.log('aspect ratio: %s' % aspect_ratio)
-        should_zoom = getAspectRatio(xbmc) in ASPECT_RATIO_TO_ZOOM
+        should_zoom = aspect_ratio in ASPECT_RATIO_TO_ZOOM
         xbmc.log('should zoom: %s' % should_zoom)
-        if sys.argv[1] == 'zoom' and should_zoom:
+        if should_zoom:
             xbmc.log('attempting to zoom')
-            setZoomNonScope(xbmc)
-        if sys.argv[1] == 'reset' and should_zoom:
-            xbmc.log('attempting to reset')
             setZoomNonScope(xbmc)
     except:
         xbmc.log("Unexpected error: %s" % sys.exc_info()[0])
